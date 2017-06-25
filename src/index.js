4bb91fe0466f9164078e47a6d0e54778fb5071d2
ZOODUCK <zooduck@beniceoreatsomerice.com>
2017-06-25 06:02:44 +0100

    mock translations table and controls

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import Services from "./scripts/services.js";
 
 Services.log();
 Services.es6BabelTest();
+
+const FILE_INPUT = document.getElementById("file");
+FILE_INPUT.addEventListener("change", (e) => {
+	alert(e);
+});
