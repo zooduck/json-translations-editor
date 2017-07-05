96f8ecf3155208204f2f21adc97b56809f088933
ZOODUCK (Wesley Pumpkinhead) <zooduck@BeNiceOrEatSomeRice.com>
2017-07-05 16:49:02 +0100

    delete services - now moved to services subdir

--- src/scripts/json_validation_service.js
+++
@@ -1,9 +0,0 @@
-export function isValidJSON (data) {
-    try {
-        JSON.parse(data);
-    }
-    catch (error) {
-        return {valid: false, error: error.message}
-    }
-    return {valid: true, error: null}
-}
